b6c6ccd9f9be204f9a679ed6be70517802648a8e
Manish Poduval <manishpoduval@hotmail.com>
2019-06-09 12:08:32 +0530

    honest de server changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,22 +11,22 @@ router.post('/location', function(req, res, next) {
    console.log(req.body)
    let locName = 'Not Found';
    locationPolygons.features.forEach((feature) => {
-          const {geometry: {coordinates}, properties: {name}} =  feature;
-          if (coordinates[0].length) {
-            coordinates[0].forEach((feature) => {
-              const { lat, lng } = req.body.location
-              if (Number(lng.toFixed(2) === coordinates[0] && Number(lat.toFixed(2)) == coordinates[1] )) {
-                locName = name;
-              }
-            })
-          }
-          else {
-            const { lat, lng } = req.body.location
-              if (Number(lng.toFixed(2) === coordinates[0] && Number(lat.toFixed(2)) == coordinates[1] )) {
-                locName = name;
-              }
-          }
-   })
+    const {geometry: {coordinates}, properties: {Name}} =  feature;
+    if (coordinates[0].length) {
+      coordinates[0].forEach((coOrdinate) => {
+        const { lat, lng } = req.body.location;
+        if (Number(lng.toFixed(2) == Number(coOrdinate[0].toFixed(2)) && Number(lat.toFixed(2)) == Number(coOrdinate[1].toFixed(2)) )) {
+          locName = Name;
+        }
+      })
+    }
+    else {
+      const { lat, lng } = req.body.location;
+        if (Number(lng.toFixed(2) == Number(coordinates[0].toFixed(2)) && Number(lat.toFixed(2)) == Number(coordinates[1].toFixed(2)) )) {
+          locName = Name;
+        }
+    }
+})
    res.json({locName});
 });
 
