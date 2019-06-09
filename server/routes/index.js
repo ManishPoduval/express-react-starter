69b9cf278d85e51c065318f68d9da87c634fd8f1
Manish Poduval <manishpoduval@hotmail.com>
2019-06-09 11:54:02 +0530

    honest de

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
-router.get('/message', function(req, res, next) {
-  res.json('Welcome To React');
+
+let rawdata = fs.readFileSync('./FullStackTest_DeliveryAreas.json');  
+let locationPolygons = JSON.parse(rawdata);  
+
+
+router.post('/location', function(req, res, next) {
+   console.log(req.body)
+   let locName = 'Not Found';
+   locationPolygons.features.forEach((feature) => {
+          const {geometry: {coordinates}, properties: {name}} =  feature;
+          if (coordinates[0].length) {
+            coordinates[0].forEach((feature) => {
+              const { lat, lng } = req.body.location
+              if (Number(lng.toFixed(2) === coordinates[0] && Number(lat.toFixed(2)) == coordinates[1] )) {
+                locName = name;
+              }
+            })
+          }
+          else {
+            const { lat, lng } = req.body.location
+              if (Number(lng.toFixed(2) === coordinates[0] && Number(lat.toFixed(2)) == coordinates[1] )) {
+                locName = name;
+              }
+          }
+   })
+   res.json({locName});
 });
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -3,27 +3,83 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { message: '' };
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { 
+      message: '',
+      place: '',
+      error: false,
+      errorMessage: '',
+      responseReturned: false,
+      locName: '',
+    };
   }
 
-  componentDidMount() {
-    fetch('/api/message')
+  handleChange = (event) => {
+    this.setState({
+      place: event.target.value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    this.setState({
+      responseReturned: false,
+      error: false,
+    })
+    const key = 'AIzaSyDFbWCbxnezS91Cd3HP7VeNzLLwlBY3ZQk'
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.place}&key=${key}`
+        fetch(url)
       .then(response => response.json())
-      .then(json => this.setState({ message: json }));
+      .then(json => {
+        const {results, error_message} = json
+
+        if (results && results.length) {
+            const locationObj = results[0].geometry.location
+            console.log(locationObj)
+            var data = {location: locationObj};
+              fetch('/api/location', {
+                method: 'POST', 
+                body: JSON.stringify(data), 
+                headers:{
+                  'Content-Type': 'application/json'
+                }
+              })
+              .then(response => response.json())
+              .then(json => this.setState({
+                 locName: json.locName,
+                 responseReturned: true,
+                 }));
+        }
+        else {
+          this.setState({
+            error: true,
+            errorMessage: error_message
+          })
+        }
+      });
+    event.preventDefault();
   }
 
   render() {
+    const {error, errorMessage, responseReturned, locName} = this.state;
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>{this.state.message}</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Enter Place:
+          <input type="text" value={this.state.place} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {
+        error ? <div className="errorDiv">{errorMessage}</div> : null
+      }
+      {
+        responseReturned ? <div>Output: {locName}</div> : null
+      }
       </div>
     );
   }
